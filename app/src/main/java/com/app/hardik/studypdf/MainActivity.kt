b442016aae58502fa6c08072e85135b84a17a726
Hardik05 <hardikarekar05@gmail.com>
2020-02-26 18:40:34 +0530

    animation added to spalsh screen

--- app/src/main/java/com/app/hardik/studypdf/MainActivity.kt
+++ app/src/main/java/com/app/hardik/studypdf/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.app.hardik.studypdf
 //Splashscreen
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Intent
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+            //Global declaration of variables
+            lateinit var anim : Animation
+            lateinit var logo : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            //Assigning ID's of Views to Variables
+            logo = findViewById(R.id.logo)
+
+            //Create Animation
+            anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in)
+
+            //Animation Class functions
+            anim.setAnimationListener(object : AnimationListener {
+            override fun onAnimationStart(animation: Animation) {
+            }
+            override fun onAnimationEnd(animation: Animation) {
+                startActivity(Intent(this@MainActivity, userscreen::class.java))
+                // userscreen.class is the activity to go after showing the splash screen.
+            }
+            override fun onAnimationRepeat(animation: Animation) {
+            }
+        })
+
+            //Trigger Animation
+            logo.startAnimation(anim)
     }
 }
