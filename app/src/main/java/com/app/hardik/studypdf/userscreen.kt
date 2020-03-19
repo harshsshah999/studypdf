de4fed0707c14c81b90fa34f6c74cc5a9fe4b61a
Abhishek <abhishek1507.gorivale@gmail.com>
2020-03-19 19:40:11 +0530

    Added some animation and changes in Intro Slides

--- app/src/main/java/com/app/hardik/studypdf/userscreen.kt
+++ app/src/main/java/com/app/hardik/studypdf/userscreen.kt
@@ -18,45 +18,69 @@ class userscreen : AppCompatActivity() {
         val welcometxt = findViewById<TextView>(R.id.textView2)
         val seek = findViewById<SeekBar>(R.id.seekBar)
         val nextbtn = findViewById<Button>(R.id.button2)
-        var done :Int = 0
-        lateinit var anim : Animation
+        val leftslidebtn = findViewById<Button>(R.id.button3)
+        val rightslidebtn = findViewById<Button>(R.id.button5)
+        lateinit var animl : Animation
+        lateinit var animr : Animation
 
         //Create Animation
-        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.to_left)
+        animl = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.to_left)
+        animr = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.to_right)
 
         //function for changing images and texts
+        fun leftanim(){
+            //Trigger Left Animation
+            welcomeimg.startAnimation(animl)
+            welcometxt.startAnimation(animl)
+            return
+        }
+        fun rightanim(){
+            //Trigger Right Animation
+            welcomeimg.startAnimation(animr)
+            welcometxt.startAnimation(animr)
+            return
+        }
         fun change(){
-            if (seek.progress == 1){
-                //Trigger Animation
-                welcomeimg.startAnimation(anim)
-                welcometxt.startAnimation(anim)
+            if (seek.progress == 0){
                 welcomeimg.setImageResource(R.drawable.ic_launcher_background)
                 welcometxt.setText("Introtxt1")
+                return
             }
             else{
-                if (seek.progress == 2){
-                    welcomeimg.startAnimation(anim)
-                    welcometxt.startAnimation(anim)
+                if (seek.progress == 1){
                     welcomeimg.setImageResource(R.drawable.appintro_indicator_dot_grey)
                     welcometxt.setText("Introtxt2")
+                    return
                 }
                 else{
-                    welcomeimg.startAnimation(anim)
-                    welcometxt.startAnimation(anim)
-                    welcomeimg.setImageResource(R.drawable.appintro_indicator_dot_white)
-                    welcometxt.setText("Introtxt3")
+                    if  (seek.progress == 2) {
+                        welcomeimg.setImageResource(R.drawable.appintro_indicator_dot_white)
+                        welcometxt.setText("Introtxt3")
+                        return
+                    }
+                    else{
+                        welcomeimg.setImageResource(R.drawable.ic_appintro_done_white)
+                        welcometxt.setText("Introtxt4")
+                        return
+                    }
                 } }
         }
 
-        //function for nextslide
+        //function for next slide & previous slide
         fun nextslide(){
-            //if (done == 1){intent}
+            if(seek.progress != seek.max){
             seek.progress = seek.progress + 1
-            if (seek.progress==seek.max){nextbtn.text = "Done"
-                done =1
-            }
-            change()
+            leftanim()
+            change()}
+            if (seek.progress==seek.max){nextbtn.text = "Done" }
          }
+        fun previousslide(){
+            if(seek.progress != seek.min){
+            seek.progress = seek.progress - 1
+            rightanim()
+            change()
+            }
+        }
 
         //seekbar class creation
         seek?.setOnSeekBarChangeListener(object :SeekBar.OnSeekBarChangeListener{
@@ -76,8 +100,7 @@ class userscreen : AppCompatActivity() {
 
         //onClick listeners for images,texts,buttons
         nextbtn.setOnClickListener { nextslide() }
-        welcomeimg.setOnClickListener { nextslide() }
-        welcometxt.setOnClickListener { nextslide() }
-
+        rightslidebtn.setOnClickListener {previousslide()}
+        leftslidebtn.setOnClickListener {nextslide()}
     }
 }
