6499f017614a96fed4c9bd84ba9db05d45e7d3d3
Hardik05 <hardikarekar05@gmail.com>
2020-05-09 19:24:52 +0530

    Changes in homepage and added some scrap codes for testing backend

--- app/src/main/java/com/app/hardik/studypdf/HomeFragment.kt
+++ app/src/main/java/com/app/hardik/studypdf/HomeFragment.kt
@@ -1,15 +1,21 @@
 package com.app.hardik.studypdf
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.fragment.app.Fragment
+import com.google.firebase.database.*
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+lateinit var dbref: DatabaseReference
 
 /**
  * A simple [Fragment] subclass.
@@ -33,8 +39,39 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        dbref = FirebaseDatabase.getInstance().getReference("Transactions")
+        var view = inflater.inflate(R.layout.fragment_home, container, false)
+        var customers = ArrayList<String>()
+        var adapter = ArrayAdapter<String>(context,
+            android.R.layout.simple_list_item_1,
+            customers
+            )
+       var transactionData = view.findViewById<ListView>(R.id.transactions)
+        transactionData.adapter = adapter
+        dbref.addChildEventListener(object : ChildEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+            }
+
+            override fun onChildChanged(p0: DataSnapshot, p1: String?) {
+            }
+
+            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+                var values = p0.getValue(Transactions::class.java).toString()
+                Log.i("datasnap",p0.toString())
+                customers.add(values)
+                adapter.notifyDataSetChanged()
+
+            }
+
+            override fun onChildRemoved(p0: DataSnapshot) {
+            }
+
+        })
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return view
     }
 
     companion object {
--- app/src/main/java/com/app/hardik/studypdf/userdashboard.kt
+++ app/src/main/java/com/app/hardik/studypdf/userdashboard.kt
@@ -2,12 +2,75 @@ package com.app.hardik.studypdf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 
 class userdashboard : AppCompatActivity() {
+    lateinit var database: FirebaseDatabase
+    lateinit var databaseReference: DatabaseReference
+    lateinit var userauth : FirebaseAuth
+    lateinit var name : String
+    lateinit var helloname : TextView
+    var user : FirebaseUser? = null
+
+    //Scrap Buttons
+    lateinit var se : Button
+    lateinit var spcc : Button
+    lateinit var tcs : Button
+    lateinit var css : Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_userdashboard)
+        helloname = findViewById(R.id.welcome)
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database.getReference()
+        userauth = FirebaseAuth.getInstance()
+        user = userauth.currentUser
+        databaseReference.child("Users").child("Students").child(user!!.uid).child("Username").addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+            }
 
+            override fun onDataChange(p0: DataSnapshot) {
+                 name = p0.value.toString()
+                helloname.text ="Welcome " + name
+            }
+
+        })
+
+
+
+
+        //Testing Code , Need to be Scrapped after testing. Buttons will NOT be the options for the PDF's.
+        // We will have a ListView for that
+        se = findViewById(R.id.SE)
+        spcc = findViewById(R.id.SPCC)
+        tcs = findViewById(R.id.TCS)
+        css = findViewById(R.id.CSS)
+
+    }
+    fun pdfwrite(pdf:String,value:String){
+        databaseReference.child("Transactions").child(user!!.uid).child("name").setValue(name)
+        databaseReference.child("Transactions").child(user!!.uid).child("pdf").setValue(pdf)
+        databaseReference.child("Transactions").child(user!!.uid).child("value").setValue(value)
+
+    }
+    fun onClick(v: View){
+        when (v.id){
+            R.id.SE ->pdfwrite("SE","50 RS")
+            R.id.SPCC ->pdfwrite("SPCC","70 RS")
+            R.id.TCS ->pdfwrite("TCS","100 RS")
+            R.id.CSS ->pdfwrite("CSS","90 RS")
+            else -> {
+
+            }
+        }
+        Toast.makeText(applicationContext,"PDF Bought Successfully",Toast.LENGTH_SHORT).show()
     }
 }
