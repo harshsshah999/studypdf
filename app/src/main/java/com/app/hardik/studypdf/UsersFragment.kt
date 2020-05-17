a615ff1479104b43a9bddf33fb7778a101348c1e
Abhishek <abhishek1507.gorivale@gmail.com>
2020-05-17 18:31:00 +0530

    Added card view in user fragment

--- app/src/main/java/com/app/hardik/studypdf/UsersFragment.kt
+++ app/src/main/java/com/app/hardik/studypdf/UsersFragment.kt
@@ -1,10 +1,18 @@
 package com.app.hardik.studypdf
 
+import android.app.ActionBar
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import kotlinx.android.synthetic.main.fragment_users.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +25,8 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class UsersFragment : Fragment() {
+    lateinit var create : Button
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +44,61 @@ class UsersFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false)
+        val view: View = inflater!!.inflate(R.layout.fragment_users, container, false)
+        create=view.findViewById(R.id.createcard)
+        // Set a click listener for button widget
+        create.setOnClickListener{
+
+            // Initialize a new CardView instance
+            val card_view = CardView(view?.context)
+
+            // Initialize a new LayoutParams instance, CardView width and height
+            val layoutParams = ActionBar.LayoutParams(
+                ActionBar.LayoutParams.MATCH_PARENT, // CardView width
+                ActionBar.LayoutParams.WRAP_CONTENT // CardView height
+            )
+
+            // Set bottom margin for card view
+            layoutParams.bottomMargin = 50
+
+            // Set the card view layout params
+            card_view.layoutParams = layoutParams
+
+            // Set the card view corner radius
+            card_view.radius = 12F
+
+            // Set the card view content padding
+            card_view.setContentPadding(25,25,25,25)
+
+            // Set the card view background color
+            card_view.setCardBackgroundColor(Color.LTGRAY)
+
+            // Set card view elevation
+            card_view.cardElevation = 8F
+
+            // Set card view maximum elevation
+            card_view.maxCardElevation = 12F
+
+            // Set a click listener for card view
+            card_view.setOnClickListener{
+                Toast.makeText(
+                    view.context,
+                    "Card clicked.",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+            // Add an ImageView to the CardView
+            card_view.addView(generateImageView())
+            card_view.addView(generatename())
+            card_view.addView(generatetitle())
+            card_view.addView(generatedate())
+            card_view.addView(generateprice())
+
+            // Finally, add the CardView in root layout
+            root2.addView(card_view)
+        }
+
+        return view
     }
 
     companion object {
@@ -56,4 +120,52 @@ class UsersFragment : Fragment() {
                 }
             }
     }
+
+    // Custom method to generate an image view
+    private fun generateImageView(): ImageView {
+        val imageView = ImageView(view?.context)
+        val params = ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, 250)
+        imageView.layoutParams = params
+        imageView.setImageResource(R.drawable.ic_launcher_background)
+        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+        return imageView
+    }
+    //Custom method to generate an text view
+    private fun generatename(): TextView {
+        val textView = TextView(view?.context)
+        val params = ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT )
+        params.leftMargin = 300
+        textView.layoutParams = params
+        textView.text = "Abhishek"
+        textView.textSize = 22F
+        return textView
+    }
+    private fun generatetitle(): TextView {
+        val textView = TextView(view?.context)
+        val params = ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT )
+        params.setMargins(300,70,0,0)
+        textView.layoutParams = params
+        textView.text = "SPCC.pdf"
+        textView.textSize = 20F
+        return textView
+    }
+    private fun generatedate(): TextView {
+        val textView = TextView(view?.context)
+        val params = ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT )
+        params.setMargins(300,210,0,0)
+        textView.layoutParams = params
+        textView.text = "20/2/2020"
+        textView.textSize = 20F
+        return textView
+    }
+    private fun generateprice(): TextView {
+        val textView = TextView(view?.context)
+        val params = ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT )
+        params.setMargins(300,140,0,0)
+        textView.layoutParams = params
+        textView.text = "Price : 40 Rs"
+        textView.textSize = 20F
+        return textView
+    }
+
 }
